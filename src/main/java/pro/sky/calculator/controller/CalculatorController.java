33dfcb4215f99aa6cce9191e299d381677b9c370
Homework-10 <danilovsansan@gmail.com>
2024-03-03 03:26:15 +0300

    Добавил Interface

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.calculator.service.CalculatorService;
 
+
 @RestController
 public class CalculatorController {
-    private  final CalculatorService calculatorService ;
+    private final CalculatorService calculatorService;
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
@@ -20,18 +21,22 @@ public class CalculatorController {
 
     @GetMapping("/calculator/plus")
     public String pumSumm(@RequestParam("num1") Integer pum1, @RequestParam("num2") Integer pum2) {
-        return calculatorService.numSumm(pum1,pum2);
+        return calculatorService.numSumm(pum1, pum2);
     }
+
     @GetMapping("/calculator/minus")
     public String pumMinus(@RequestParam("num1") Integer pum1, @RequestParam("num2") Integer pum2) {
-        return calculatorService.numMinus(pum1,pum2);
+        return calculatorService.numMinus(pum1, pum2);
     }
+
     @GetMapping("/calculator/multiply")
     public String pumMultiply(@RequestParam("num1") Integer pum1, @RequestParam("num2") Integer pum2) {
-        return calculatorService.numMultiply(pum1,pum2);
-    }@GetMapping("/calculator/divide")
+        return calculatorService.numMultiply(pum1, pum2);
+    }
+
+    @GetMapping("/calculator/divide")
     public String pumDivide(@RequestParam("num1") Double pum1, @RequestParam("num2") Double pum2) {
-        return calculatorService.numDivide(pum1,pum2);
+        return calculatorService.numDivide(pum1, pum2);
     }
 
     @GetMapping("/counter")
--- src/main/java/pro/sky/calculator/service/CalculatorService.java
+++ src/main/java/pro/sky/calculator/service/CalculatorService.java
@@ -1,60 +1,17 @@
 package pro.sky.calculator.service;
 
-import org.springframework.stereotype.Service;
+public interface CalculatorService {
+    void countdown();
+    int getCount();
 
-@Service
-public class CalculatorService {
-    private int count;
+    String hello();
 
-    public void countdown() {
-        this.count++;
-    }
+    String numSumm(Integer num1, Integer num2);
 
-    public int getCount() {
-        return this.count;
-    }
+    String numMinus(Integer num1, Integer num2);
 
-    public String hello() {
-        return "Добро пожаловать в калькулятор";
-    }
+    String numMultiply(Integer num1, Integer num2);
 
-    public String numSumm(Integer num1, Integer num2) {
-        if (num1 != null && num2 != null) {// непойму почему непроверяется на null Пробовал и через != тоже ничего невыводит.
-            int num3 = num1 + num2;
-            return num1 + "+" + num2 + "=" + num3;
-        }
-        return "Введите коректные значения!!";
-    }
-
-
-    public String numMinus(Integer num1, Integer num2) {
-        if (num1 != null && num2 != null) {// непойму почему непроверяется на null Пробовал и через != тоже ничего невыводит.
-            int num3 = num1 - num2;
-            return num1 + "-" + num2 + "=" + num3;
-        }
-        return "Введите коректные значения!!";
-    }
-
-    public String numMultiply(Integer num1, Integer num2) {
-        if (num1 != null && num2 != null) {// непойму почему непроверяется на null Пробовал и через != тоже ничего невыводит.
-            int num3 = num1 * num2;
-            {
-                return num1 + "*" + num2 + "=" + num3;
-            }
-        }
-        return "Введите коректные значения!!";
-    }
-
-    public String numDivide(Double num1, Double num2) {
-        if (num1 .equals("") | num2.equals("")| num2 ==0) {// непойму почему непроверяется на null Пробовал и через != тоже ничего невыводит.
-            return "Введите коректные значения!!";
-        }
-        double num3 = num1 / num2;
-        return num1 + "/" + num2 + "=" + num3;
-    }
-
-    public String count() {
-        countdown();
-        return "Количество запросов: " + getCount();
-    }
+    String numDivide(Double num1, Double num2);
+    String count();
 }
